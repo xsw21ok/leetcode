d528ed964fc3cd1afa25909afe0c37a56978285a
Se7en.Xu <xushiwen008@163.com>
2018-09-22 09:56:00 +0800

    两数之和

--- Leetcode/src/com/algorithm/SumOfTwoNumbers.java
+++ Leetcode/src/com/algorithm/SumOfTwoNumbers.java
@@ -1,5 +1,30 @@
 package com.algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		SumOfTwoNumbers numbers = new SumOfTwoNumbers();
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] result = numbers.twoSum(nums, target);
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), i };
+			}
+			map.put(nums[i], i);
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
 
 }
